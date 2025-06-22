7c8239e35f33b820a1312e07818ea35d71624d66
@exeProcess <ajani_habeeb88@yahoo.com>
2025-06-22 12:18:38 +0100

    publish

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -110,10 +110,10 @@ exports.register = async (req, res) => {
     }
 
     // Verify BVN with Dojah
-    // const response = await dojah.verifyBVN(bvn);
-    // if (!response || !response.entity) {
-    //   return res.status(400).json({ error: 'Invalid BVN' });
-    // }
+    const response = await dojah.verifyBVN(bvn);
+    if (!response || !response.data || !response.data.entity) {
+      return res.status(400).json({ error: 'Invalid BVN' });
+    }
 
     // const entity = response.data.entity;
     // const {
@@ -215,7 +215,7 @@ exports.register = async (req, res) => {
 
     //const token = jwt.generateToken(user);
     return res.status(201).json({
-      bvn
+      response: response.data
     });
     // res.status(201).json({
     //   token,
