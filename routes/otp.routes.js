eeb2551f038335894e67cf8100ba7f0453ae2e44
@exeProcess <ajani_habeeb88@yahoo.com>
2025-06-22 02:56:34 +0100

    publish

--- routes/otp.routes.js
+++ routes/otp.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const otpController = require('../controllers/otp.controller');
 
-router.post('/send', otpController.sendOtp);
+router.post('/send', otpController.sendSMS);
 router.post('/verify', otpController.verifyOtp);
 
 module.exports = router;
