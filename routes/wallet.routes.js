83827d3489a42c9daac8f6283af89720fbdd88a9
@exeProcess <ajani_habeeb88@yahoo.com>
2025-06-29 01:00:17 +0100

    publish

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -9,8 +9,7 @@ router.post('/create-customer', auth, walletController.createCustomer);
 router.post('/update-Customer', auth, walletController.updateBankoneCustomer);
 router.get('/get-Customer', auth, walletController.getBankoneCustomer);
 router.post('/create-wallet', auth, walletController.createWallet);
-router.get('/get-customer-account-details', auth, walletController.getCustomerAccountDetails);
-router.get('/get-bankone-account-details', auth, walletController.getBankoneAccountDetails);
+router.get('/get-customer-account-details', auth, walletController.getBankoneAccountDetails);
 router.post('/reverse-transfer', auth, walletController.reverseTransfer);
 
 module.exports = router;
