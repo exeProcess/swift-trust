18fee0f3edc73dd2f9c317220e55913430c07531
@exeProcess <ajani_habeeb88@yahoo.com>
2025-06-29 13:32:04 +0100

    publish

--- routes/loan.routes.js
+++ routes/loan.routes.js
@@ -4,13 +4,13 @@ const loanController = require('../controllers/loan.controller');
 const auth = require('../middleware/auth.middleware');
 
 
-router.use(auth);
-router.post('/apply', loanController.applyForLoan);
-router.get('/:reference', loanController.getLoanInfo);
-router.get('/', loanController.getMyLoans);
-router.post('/repay', loanController.repayLoan);
-router.post('/create', loanController.createLoan);
-router.post('/create-standing-order', loanController.createStandingOrder);
+
+router.post('/apply', auth, loanController.applyForLoan);
+router.get('/:reference', auth, loanController.getLoanInfo);
+router.get('/', auth, loanController.getMyLoans);
+router.post('/repay',auth, loanController.repayLoan);
+router.post('/create', auth, loanController.createLoan);
+router.post('/create-standing-order', auth, loanController.createStandingOrder);
 router.get('/get-standing-orders', loanController.getStandingOrderDetails);
 
 module.exports = router;
