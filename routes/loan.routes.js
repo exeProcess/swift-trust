7fe62071ae79feb788aeb1fef6dee08a2233b0cb
@exeProcess <ajani_habeeb88@yahoo.com>
2025-06-29 13:33:29 +0100

    publish

--- routes/loan.routes.js
+++ routes/loan.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const loanController = require('../controllers/loan.controller');
 const auth = require('../middleware/auth.middleware');
+const loanController = require('../controllers/loan.controller');
 
 
 
