cafa81be852d07a926d11b131ac4b96f7baff6b0
@exeProcess <ajani_habeeb88@yahoo.com>
2025-07-19 22:55:47 +0100

    publish

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.json({ limit: '300mb' })); // for JSON payloads
+app.use(express.urlencoded({ extended: true, limit: '300mb' })); // for form-urlencoded (if used)
 
 const { sequelize } = require('./models');
 const authRoutes = require('./routes/auth.routes');
