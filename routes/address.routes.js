9a9abf62906a494576fdef43ef3646443d29bebe
@exeProcess <ajani_habeeb88@yahoo.com>
2025-08-03 10:23:42 +0100

    publish

--- routes/address.routes.js
+++ routes/address.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const addressController = require('../controllers/address.controller');
 const auth = require('../middleware/auth.middleware');
 
-router.post('/submit', auth, addressController.submitAddress);
+router.post('/upload-address', auth, addressController.submitAddress);
 
 module.exports = router;
