f1c72e41468e2adcb111a99144320373103fd053
@exeProcess <ajani_habeeb88@yahoo.com>
2025-08-03 09:33:12 +0100

    publish

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -179,7 +179,7 @@ exports.sendOtp = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       status: 500,
-      message: error.sendEmailOtp?.error || "Error occured"
+      message: error.message
     });
   }
 
