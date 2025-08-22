a58433bc8aad0477e75976420bd36bc35120177a
@exeProcess <ajani_habeeb88@yahoo.com>
2025-08-22 10:17:24 +0100

    publish

--- routes/loan.routes.js
+++ routes/loan.routes.js
@@ -5,12 +5,6 @@ const loanController = require('../controllers/loan.controller');
 
 
 
-router.post('/apply', auth, loanController.applyForLoan);
-router.get('/:reference', auth, loanController.getLoanInfo);
-router.get('/', auth, loanController.getMyLoans);
-router.post('/repay',auth, loanController.repayLoan);
-router.post('/create', auth, loanController.createLoan);
-router.post('/create-standing-order', auth, loanController.createStandingOrder);
-router.get('/get-standing-orders', loanController.getStandingOrders);
+router.get("/get-eligible-amount", auth, loanController.eligibleAmount);
 
 module.exports = router;
