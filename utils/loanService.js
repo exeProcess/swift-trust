51ee2998d89e1a5cc501ccf5be15bc4cd9f4ccba
@exeProcess <ajani_habeeb88@yahoo.com>
2025-08-22 10:15:34 +0100

    publish

--- utils/loanService.js
+++ utils/loanService.js
@@ -2,15 +2,26 @@
 const axios = require('axios');
 const { User, Loan, LoanRate, sequelize } = require('../models');
 
+
 exports.checkFirstTimer = async (id) => {
-  try{
-    const userLoan = await Loan.findOne({where: {userId: id}});
-    if(userLoan.status == "active"){
-      return {eligibleAmount: "0"};
+  try {
+    const userLoan = await Loan.findOne({ where: { userId: id } });
+
+    if (!userLoan) {
+      // No loan found → first timer
+      return { eligibleAmount: "100" };
     }
-    if(!userLoan){
-      return {eligibleAmount: "100"};
+
+    if (userLoan.status === "active") {
+      return { eligibleAmount: "0" };
     }
+
+    if (userLoan.status !== "active") {
+      return { eligibleAmount: "1,000,000" };
+    }
+
+  } catch (err) {
+    throw new Error(err.message);
   }
-}
+};
 
