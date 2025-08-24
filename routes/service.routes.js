45e370bcc9adaabe2a486973ea929d2f84301405
Habeeb <57136953+exeProcess@users.noreply.github.com>
2025-08-24 23:58:57 +0100

    Update service.routes.js

--- routes/service.routes.js
+++ routes/service.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const serviceController = require('../controllers/serviceController');
 const auth = require('../middleware/auth.middleware');
+const dojah = require("../utils/dojah");
 
 router.get('/get-vendors/:service', serviceController.getRemitaServiceVendors);
 router.get('/get-vendor-products/:categoryCode/:providerCode', serviceController.gerRemitaVendorProducts);
@@ -9,5 +10,5 @@ router.post('/buy-airtime', serviceController.buyAirtimeOrData);
 router.post('/buy-data', serviceController.buyAirtimeOrData); 
 router.post('/buy-electricity', serviceController.buyElectricityOrCableTvSubscription);
 router.post('/buy-cable-tv', serviceController.buyElectricityOrCableTvSubscription);
-router.get('/get-bank-list', serviceController.getRemitaBankList);
+router.get('/get-bank-list', dojah.fetchBank);
 module.exports = router;
